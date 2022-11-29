ec75d91aa12185a0df4642a65a13434d1727b3a6
xinhu.wu <xinhu.wu@unisoc.com>
2022-11-29 22:13:14 +0800

    Bug #2095475 usb: typec: modify efuse mask for cc
    [root cause  ]: modify efuse mask for cc
    [changes     ]:typec
    [side effects]:NA
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:sharkl6
    [test case]:NA
    [reviewers]:zhiyong.liu
    
    commit_template_version:v1
    
    Signed-off-by: xinhu.wu <xinhu.wu@unisoc.com>
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I241cac56f9825e82ad73498da52777a264189aa1

--- drivers/usb/typec/sc27xx_typec.c
+++ drivers/usb/typec/sc27xx_typec.c
@@ -90,7 +90,7 @@
 #define UMP518_EFUSE_CC1_SHIFT		6
 #define UMP518_EFUSE_CC2_SHIFT		11
 
-#define SC27XX_CC1_MASK(n)		GENMASK((n) + 9, (n) + 5)
+#define SC27XX_CC1_MASK(n)		GENMASK((n) + 4, (n))
 #define SC27XX_CC2_MASK(n)		GENMASK((n) + 4, (n))
 #define SC27XX_CC_SHIFT(n)		(n)
 
