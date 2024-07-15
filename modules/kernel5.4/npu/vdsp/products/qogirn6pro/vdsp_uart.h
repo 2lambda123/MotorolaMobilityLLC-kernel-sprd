d4f26343b18aa00f73168134c629ab25d8f7b75a
wufeifan <wufeifan@longcheer.com>
2024-07-15 20:22:46 +0800

    add-module
    Change-Id: I95fc1d1324546a4c0302a9e48738a22706399967

---
+++ modules/kernel5.4/npu/vdsp/products/qogirn6pro/vdsp_uart.h
@@ -0,0 +1,18 @@
+/**
+ * SPDX-FileContributor: vdsp driver uart to implement serial communication.
+ */
+ 
+struct vdsp_uart_ops;
+struct vdsp_uart_desc {
+	struct vdsp_uart_ops *ops;
+	struct regmap *mm_ahb_base;
+	struct regmap *vdsp_cfg_base;
+};
+
+struct vdsp_uart_ops {
+	int (*enable) (struct vdsp_uart_desc * ctx);
+	int (*disable) (struct vdsp_uart_desc * ctx);
+
+};
+
+struct vdsp_uart_desc *get_vdsp_uart_desc(void);
